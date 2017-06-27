6b649dedaa02570048d2883ca37f1dcbee92ae2b
fanlychie <fanlychie@yeah.net>
2017-06-27 14:45:28 +0800

    修复启动脚本

--- spring-cloud-netflix-eureka-sample/quick-start.bat
+++ spring-cloud-netflix-eureka-sample/quick-start.bat
@@ -1,6 +1,6 @@
 @echo off
 
-	TITLE autosellrobot install
+	TITLE Quick Start
 
 	COLOR 3E
 
@@ -9,7 +9,7 @@
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
-	echo [INFO] 正在构建服务器端项目 . . .
+	echo [INFO] 正在构建服务器端应用 . . .
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
@@ -23,43 +23,43 @@
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
-	echo [INFO] 正在启动服务器 Peer1 . . .
+	echo [INFO] 正在启动 Eureka 服务器 Peer1 . . .
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
 	
-	start /min cmd /k "java -jar eureka-server-0.0.1-SNAPSHOT.jar"
+	start "Eureka Peer1" /min cmd /T:3E /k "java -jar eureka-server-0.0.1-SNAPSHOT.jar"
 	
 	ping -n 5 127.1 > nul
 	
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
-	echo [INFO] 正在启动服务器 Peer2 . . .
+	echo [INFO] 正在启动 Eureka 服务器 Peer2 . . .
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
 	
-	start /min cmd /k "java -jar -Dspring.profiles.active=peer2 eureka-server-0.0.1-SNAPSHOT.jar"
+	start "Eureka Peer2" /min cmd /T:3E /k "java -jar -Dspring.profiles.active=peer2 eureka-server-0.0.1-SNAPSHOT.jar"
 	
 	ping -n 5 127.1 > nul
 	
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
-	echo [INFO] 正在启动服务器 Peer3 . . .
+	echo [INFO] 正在启动 Eureka 服务器 Peer3 . . .
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
 	
-	start /min cmd /k "java -jar -Dspring.profiles.active=peer3 eureka-server-0.0.1-SNAPSHOT.jar"
+	start "Eureka Peer3" /min cmd /T:3E /k "java -jar -Dspring.profiles.active=peer3 eureka-server-0.0.1-SNAPSHOT.jar"
 	
-	ping -n 5 127.1 > nul
+	ping -n 3 127.1 > nul
 	
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
-	echo [INFO] 正在构建客户端项目 . . .
+	echo [INFO] 正在构建客户端应用 . . .
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
@@ -70,31 +70,29 @@
 	
 	cd target
 	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在启动客户端应用 Client1 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	start "Provider Client1" /min cmd /T:3E /k "java -jar order-service-0.0.1-SNAPSHOT.jar"
+	
 	ping -n 5 127.1 > nul
 	
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
-	echo [INFO] 正在启动客户端 Client1 . . .
+	echo [INFO] 正在启动客户端应用 Client2 . . .
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
 	echo [INFO]
 	
-	start /min cmd /k "java -jar order-service-0.0.1-SNAPSHOT.jar"
+	start "Provider Client2" /min cmd /T:3E /k "java -Dspring.profiles.active=client2 -jar order-service-0.0.1-SNAPSHOT.jar"
 	
-	ping -n 5 127.1 > nul
-	
-	echo [INFO]
-	echo [INFO] ------------------------------------------------------------------------
-	echo [INFO]
-	echo [INFO] 正在启动客户端 Client2 . . .
-	echo [INFO]
-	echo [INFO] ------------------------------------------------------------------------
-	echo [INFO]
-	
-	start /min cmd /k "java -Dspring.profiles.active=client2 -jar order-service-0.0.1-SNAPSHOT.jar"
-	
-	ping -n 10 127.1 > nul
+	ping -n 20 127.1 > nul
 	
 	echo [INFO]
 	echo [INFO] ------------------------------------------------------------------------
