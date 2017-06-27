dc83763fa3612c5ebe1966cb1b703453750e1c3c
fanlychie <fanlychie@yeah.net>
2017-06-27 15:00:59 +0800

    添加快速启动脚本

---
+++ spring-cloud-netflix-ribbon-with-eureka-sample/quick-start.bat
@@ -0,0 +1,141 @@
+@echo off
+
+	TITLE Quick Start
+
+	COLOR 3E
+
+	cd %~dp0
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在构建基础模块 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	call mvn -e -ff -N install:install-file -Dfile=pom.xml -DgroupId=${project.groupId} -DartifactId=${project.artifactId} -Dversion=${project.version} -Dpackaging=pom
+	
+	cd base-module
+	
+	call mvn -e -ff clean install -Dmaven.test.skip=true
+	
+	ping -n 2 127.1 > nul
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在构建服务器端应用 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	cd ../eureka-server
+	
+	call mvn -e -ff clean package -Dmaven.test.skip=true
+	
+	cd target
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在启动 Eureka 服务器 Peer1 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	start "Eureka Peer1" /min cmd /T:3E /k "java -jar eureka-server-0.0.1-SNAPSHOT.jar"
+	
+	ping -n 5 127.1 > nul
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在启动 Eureka 服务器 Peer2 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	start "Eureka Peer2" /min cmd /T:3E /k "java -jar -Dspring.profiles.active=peer2 eureka-server-0.0.1-SNAPSHOT.jar"
+	
+	ping -n 5 127.1 > nul
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在构建服务提供者应用 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	cd ../../order-service
+	
+	call mvn -e -ff clean package -Dmaven.test.skip=true
+	
+	cd target
+	
+	ping -n 2 127.1 > nul
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在启动服务提供者应用 Client1 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	start "Provider Client1" /min cmd /T:3E /k "java -jar order-service-0.0.1-SNAPSHOT.jar"
+	
+	ping -n 5 127.1 > nul
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在启动服务提供者应用 Client2 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	start "Provider Client2" /min cmd /T:3E /k "java -Dspring.profiles.active=client2 -jar order-service-0.0.1-SNAPSHOT.jar"
+	
+	ping -n 5 127.1 > nul
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在构建服务消费者应用 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	cd ../../user-center
+	
+	call mvn -e -ff clean package -Dmaven.test.skip=true
+	
+	cd target
+	
+	ping -n 2 127.1 > nul
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 正在启动服务消费者应用 . . .
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	
+	start "Consumer" /min cmd /T:3E /k "java -jar user-center-0.0.1-SNAPSHOT.jar"
+	
+	ping -n 5 127.1 > nul
+	
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+	echo [INFO] 构建完毕！
+	echo [INFO]
+	echo [INFO] http://localhost:8888/user/orders/1001
+	echo [INFO]
+	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]
+
+pause
